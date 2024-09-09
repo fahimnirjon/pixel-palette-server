8d098c4db762cf699b9400e0068e993bb6ec0182
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-09 23:08:51 +0600

    added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
